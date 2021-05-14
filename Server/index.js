28d037b5f5ce878d902bf83c3963515cb1dbab3f
ByungJin-Lee <Byungjin.dev@gmail.com>
2021-05-14 22:50:47 +0900

    1.0.1

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,41 @@
 //index.js
 var express = require('express');
-var app = express();
+const app = express();
 
-const port = 80;
+const port = 3000;
 
-
-app.get('/', function(req,res){
+app.get('/', (req,res)=>{
     res.send("hello");
+});
+
+app.get('/users', (req,res)=>{
+    return res.json(users);
+});
+
+app.get('/gps_data/:mac', (req,res)=>{    
+    const mac = req.params.mac;
+    if(mac.length > 0){
+        //ToDo
+    }else{
+        return "There is No Data by MAC("+mac+")";
+    }
 });
 
 app.listen(port, function(){
     console.log("server on! http://localhost:"+port);
 });
+
+let users = [
+    {
+        id : 1,
+        name : "Hyne",
+    },
+    {
+        id : 2,
+        name : "ne",
+    },
+    {   
+        id : 3,
+        name : "sdfsfd",
+    }
+]
